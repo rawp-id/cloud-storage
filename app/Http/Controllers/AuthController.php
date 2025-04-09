02e1aef1967a8b9da72ba25596c5071d7190c040
rawp-id <wpipit35@gmail.com>
2025-04-09 18:32:07 +0700

    update all

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,23 +7,29 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
+        // dd($request->all());
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
 
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
-            'access_key'=> bin2hex(random_bytes(16)),
-            'secret_key'=> bin2hex(random_bytes(32)),
+            'access_key' => bin2hex(random_bytes(16)),
+            'secret_key' => bin2hex(random_bytes(32)),
         ]);
 
-        return response()->json(['message' => 'User registered successfully'], 201);
+        return response()->json([
+            'message' => 'User registered successfully',
+            // 'user' => $user,
+            'access_key' => $user->access_key,
+            'access_token' => $user->secret_key,
+        ], 201);
     }
-
-    
 }
